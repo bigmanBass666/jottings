d20046a6fc689c10c0e7c683281f30aa6d7dbfc4
bigmanBass666 <3858351104@qq.com>
2024-12-17 21:31:40 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'gitee-origin/main'.
    
    Changes to be committed:
    	modified:   .vitepress/cache/deps/_metadata.json
    	modified:   .vitepress/configs/search.js

--- .vitepress/configs/search.js
+++ .vitepress/configs/search.js
@@ -5,22 +5,22 @@ export const search = {
   options: {
     translations: {
       button: {
-        buttonText: '搜索文档',
-        buttonAriaLabel: '搜索文档'
+        buttonText: '搜索',
+        buttonAriaLabel: '搜索'
       },
       modal: {
-        displayDetails: '显示详情',
-        resetButtonTitle: '清除查询条件',
+        displayDetails: '详情',
+        resetButtonTitle: '清除',
         backButtonTitle: '返回',
-        noResultsText: '无法找到相关结果',
+        noResultsText: '无',
         footer: {
           selectText: '选择',
-          selectKeyAriaLabel: '选择结果',
+          selectKeyAriaLabel: '选择',
           navigateText: '切换',
           navigateUpKeyAriaLabel: '向上',
           navigateDownKeyAriaLabel: '向下',
           closeText: '关闭',
-          closeKeyAriaLabel: '关闭搜索框',
+          closeKeyAriaLabel: '关闭',
         }
       }
     }
